0a2e42e76feae6b837a77beff0113b1b85e55d1c
Jules MULET <jmulet@lerebours.org>
2020-11-20 11:00:27 +0100

    Ajoutez des fichiers projet.

---
+++ GestionnaireBDD/GstBdd.cs
@@ -0,0 +1,101 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using MetierTrader;
+using System.Collections.Generic;
+
+namespace GestionnaireBDD
+{
+    public class GstBdd
+    {
+        private MySqlConnection cnx;
+        private MySqlCommand cmd;
+        private MySqlDataReader dr;
+
+        // Constructeur
+        public GstBdd()
+        {
+            string chaine = "Server=localhost;Database=bourse;Uid=root;Pwd=";
+            cnx = new MySqlConnection(chaine);
+            cnx.Open();
+        }
+
+        public List<Trader> getAllTraders()
+        {
+            List<Trader> lesTraders = new List<Trader>();
+            cmd = new MySqlCommand("select idTrader, nomTrader from trader", cnx);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Trader unTrader = new Trader(Convert.ToInt16(dr[0]), dr[1].ToString());
+                lesTraders.Add(unTrader);
+            }
+            dr.Close();
+            return lesTraders;
+        }
+        public List<ActionPerso> getAllActionsByTrader(int numTrader)
+        {
+            List<ActionPerso> lesActionsPerso = new List<ActionPerso>();
+            cmd = new MySqlCommand("select numAction, nomAction, prixAchat, quantite from acheter ar join action an on ar.numAction = an.idAction where numTrader=" + numTrader+";", cnx);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                ActionPerso uneActionPerso = new ActionPerso(Convert.ToInt16(dr[0]), dr[1].ToString(), Convert.ToDouble(dr[2]), Convert.ToInt16(dr[3]));
+                lesActionsPerso.Add(uneActionPerso);
+            }
+            dr.Close();
+            return lesActionsPerso;
+        }
+
+        public List<MetierTrader.Action> getAllActionsNonPossedees(int numTrader)
+        {
+            List<MetierTrader.Action> lesActions = new List<MetierTrader.Action>();
+            cmd = new MySqlCommand("select idAction, nomAction, coursReel from action where idAction NOT IN (select numAction from acheter where numTrader=" + numTrader + ");", cnx);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                MetierTrader.Action uneAction = new MetierTrader.Action(Convert.ToInt16(dr[0]), dr[1].ToString(), Convert.ToDouble(dr[2]));
+                lesActions.Add(uneAction);
+            }
+            dr.Close();
+            return lesActions;
+        }
+
+        public void SupprimerActionAcheter(int numAction, int numTrader)
+        {
+            cmd = new MySqlCommand("DELETE FROM `acheter` WHERE numAction=" + numAction + " AND numTrader =" + numTrader + ";", cnx);
+            cmd.ExecuteNonQuery();
+        }
+
+        public void UpdateQuantite(int numAction, int numTrader, int quantite)
+        {
+            cmd = new MySqlCommand("UPDATE `acheter` SET `quantite`=" + quantite + " WHERE numAction=" + numAction + " AND numTrader=" + numTrader + ";", cnx);
+            cmd.ExecuteNonQuery();
+        }
+
+        public double getCoursReel(int numAction)
+        {
+            double coursReel;
+            cmd = new MySqlCommand("select coursReel from action where idAction =" + numAction + ";", cnx);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            coursReel =Convert.ToDouble(dr[0]);
+            dr.Close();
+            return coursReel;
+        }
+        public void AcheterAction(int numAction, int numTrader, double prix, int quantite)
+        {
+            cmd = new MySqlCommand("INSERT INTO `acheter` (`numAction`, `numTrader`, `prixAchat`, `quantite`) VALUES ('" + numAction + "', '" + numTrader + "', '" + prix + "', '" + quantite + "');", cnx);
+            cmd.ExecuteNonQuery();
+        }
+        public double getTotalPortefeuille(int numTrader)
+        {
+            double total = 0;
+            cmd = new MySqlCommand("select sum(prixAchat*quantite) from acheter where numTrader="+ numTrader + "; ",cnx);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            total = Convert.ToDouble(dr[0]);
+            dr.Close();
+            return total;
+        }
+    }
+}
---
+++ WPFTrader/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GestionnaireBDD;
+using MetierTrader;
+
+namespace WPFTrader
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GstBdd gstBdd;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            gstBdd = new GstBdd();
+            lstTraders.ItemsSource = gstBdd.getAllTraders();
+        }
+
+        private void lstTraders_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstTraders.SelectedItem != null)
+            {
+                lstActions.ItemsSource = gstBdd.getAllActionsByTrader((lstTraders.SelectedItem as Trader).NumTrader);
+                lstActionsNonPossedees.ItemsSource = gstBdd.getAllActionsNonPossedees((lstTraders.SelectedItem as Trader).NumTrader);
+                txtTotalPortefeuille.Text = gstBdd.getTotalPortefeuille((lstTraders.SelectedItem as Trader).NumTrader).ToString();
+            }
+        }
+
+        private void lstActions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            imgAction.Source = null;
+            if(lstActions.SelectedItem!= null)
+            {
+                double coursReel = gstBdd.getCoursReel((lstActions.SelectedItem as ActionPerso).NumAction);
+                if((lstActions.SelectedItem as ActionPerso).PrixAchat > coursReel)
+                {
+                    imgAction.Source = new BitmapImage(new Uri("Images/Bas.png", UriKind.Relative));
+                }
+                else if((lstActions.SelectedItem as ActionPerso).PrixAchat == coursReel)
+                {
+                    imgAction.Source = new BitmapImage(new Uri("Images/Moyen.png", UriKind.Relative));
+                }
+                else
+                {
+                    imgAction.Source = new BitmapImage(new Uri("Images/Haut.png", UriKind.Relative));
+                }
+            }
+        }
+
+        private void btnVendre_Click(object sender, RoutedEventArgs e)
+        {
+            bool isConvertible = true;
+            try
+            {
+                Convert.ToInt16(txtQuantiteVendue.Text);
+            }
+            catch (Exception)
+            {
+                isConvertible = false;
+                MessageBox.Show("Merci d'entrer des valeurs valables");
+            }
+            if (isConvertible)
+            {
+                if (lstActions.SelectedItem == null)
+                {
+                    MessageBox.Show("Merci de choisir une action");
+                }
+                else if (Convert.ToInt16(txtQuantiteVendue.Text) > (lstActions.SelectedItem as ActionPerso).Quantite)
+                {
+                    MessageBox.Show("Merci de choisir un quantité à vendre valide");
+                }
+                else
+                {
+
+                    if (Convert.ToInt16(txtQuantiteVendue.Text) < (lstActions.SelectedItem as ActionPerso).Quantite)
+                    {
+                        gstBdd.UpdateQuantite((lstActions.SelectedItem as ActionPerso).NumAction, (lstTraders.SelectedItem as Trader).NumTrader, (lstActions.SelectedItem as ActionPerso).Quantite - Convert.ToInt16(txtQuantiteVendue.Text));
+                    }
+                    else
+                    {
+                        gstBdd.SupprimerActionAcheter((lstActions.SelectedItem as ActionPerso).NumAction, (lstTraders.SelectedItem as Trader).NumTrader);
+                    }
+                    lstActions.ItemsSource = null;
+                    lstActions.ItemsSource = gstBdd.getAllActionsByTrader((lstTraders.SelectedItem as Trader).NumTrader);
+                    lstActionsNonPossedees.ItemsSource = null;
+                    lstActionsNonPossedees.ItemsSource = gstBdd.getAllActionsNonPossedees((lstTraders.SelectedItem as Trader).NumTrader);
+                }
+            }
+        }
+
+        private void btnAcheter_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstActionsNonPossedees.SelectedItem == null)
+            {
+                MessageBox.Show("Merci de choisir une action");
+            }
+            else if (txtPrixAchat.Text == "")
+            {
+                MessageBox.Show("Merci d'entrer un prix d'achat");
+            }
+            else if(txtQuantiteAchetee.Text == "")
+            {
+                MessageBox.Show("Merci d'entrer une quantité valable");
+            }
+            else
+            {
+                bool isConvertible = true;
+                try
+                {
+                    Convert.ToInt16(txtQuantiteAchetee.Text);
+                    Convert.ToDouble(txtPrixAchat.Text);
+                }
+                catch (Exception)
+                {
+                    isConvertible = false;
+                    MessageBox.Show("Merci d'entrer des valeurs valables");
+                }
+                if (isConvertible)
+                {
+                    gstBdd.AcheterAction((lstActionsNonPossedees.SelectedItem as MetierTrader.Action).NumAction, (lstTraders.SelectedItem as Trader).NumTrader, Convert.ToDouble(txtQuantiteAchetee.Text), Convert.ToInt16(txtQuantiteAchetee.Text));
+                    lstActions.ItemsSource = null;
+                    lstActions.ItemsSource = gstBdd.getAllActionsByTrader((lstTraders.SelectedItem as Trader).NumTrader);
+                    lstActionsNonPossedees.ItemsSource = null;
+                    lstActionsNonPossedees.ItemsSource = gstBdd.getAllActionsNonPossedees((lstTraders.SelectedItem as Trader).NumTrader);
+                }
+            }
+        }
+    }
+}
